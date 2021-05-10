c32bc0df709822ee107f85615db9ce71f3d91970
Romain Rigaux <romain.rigaux@gmail.com>
2021-05-10 13:46:08 -0700

    [tests] Adding the interpreter

--- tests/editor/query/engines_test.py
+++ tests/editor/query/engines_test.py
@@ -19,29 +19,36 @@ from unittest.mock import patch
 
 import pytest
 
+from compose.editor.api import _get_interpreter
 from compose.editor.query.engines import Executor
 
 
 @pytest.mark.django_db
 def test_execute():
+    interpreter = _get_interpreter(None)
 
     with patch("compose.editor.query.engines.SqlAlchemyInterface.execute") as execute:
         execute.return_value = {"guid": "abc"}
 
-        data = Executor(username="test").execute(statement="SELECT 1, 2, 3")
+        data = Executor(username="test", interpreter=interpreter).execute(
+            statement="SELECT 1, 2, 3"
+        )
 
         assert data["handle"].get("guid") == "abc"
 
 
 @pytest.mark.django_db
 def test_check_status():
+    interpreter = _get_interpreter(None)
 
     with patch(
         "compose.editor.query.engines.SqlAlchemyInterface.check_status"
     ) as check_status:
         check_status.return_value = {"status": "running"}
 
-        data = Executor(username="test").check_status(query_id="abc")
+        data = Executor(username="test", interpreter=interpreter).check_status(
+            query_id="abc"
+        )
 
         assert data["status"] == "running"
 
@@ -52,11 +59,14 @@ def test_execute_query_flow():
     Test flow with Compose handle (non DB native handle).
     Compose Editor API (non native DB api)
     """
+    interpreter = _get_interpreter(None)
 
     with patch("compose.editor.query.engines.SqlAlchemyInterface.execute") as execute:
         execute.return_value = {"guid": "abc"}
 
-        data = Executor(username="test").execute(statement="SELECT 1, 2, 3")
+        data = Executor(username="test", interpreter=interpreter).execute(
+            statement="SELECT 1, 2, 3"
+        )
 
         assert data["handle"].get("guid") == "abc"
 
@@ -65,7 +75,9 @@ def test_execute_query_flow():
     ) as check_status:
         check_status.return_value = {"status": "running"}
 
-        data = Executor(username="test").check_status(query_id="abc")
+        data = Executor(username="test", interpreter=interpreter).check_status(
+            query_id="abc"
+        )
 
         assert data["status"] == "running"
 
@@ -76,7 +88,9 @@ def test_execute_query_flow():
     ) as check_status:
         check_status.return_value = {"status": "ready"}
 
-        data = Executor(username="test").check_status(query_id="abc")
+        data = Executor(username="test", interpreter=interpreter).check_status(
+            query_id="abc"
+        )
 
         assert data["status"] == "ready"
 
@@ -85,7 +99,9 @@ def test_execute_query_flow():
     ) as fetch_result:
         fetch_result.return_value = {"data": [[1]], "meta": [["C1"]]}
 
-        data = Executor(username="test").fetch_result(query_id="abc")
+        data = Executor(username="test", interpreter=interpreter).fetch_result(
+            query_id="abc"
+        )
 
         assert {"data": [[1]], "meta": [["C1"]]} == data["result"]
 
@@ -99,7 +115,9 @@ def test_execute_query_flow():
 @pytest.mark.live
 @pytest.mark.django_db
 def test_execute_query_flow_live():
-    executor = Executor(username="test")
+    interpreter = _get_interpreter(None)
+
+    executor = Executor(username="test", interpreter=interpreter)
 
     data = executor.execute(statement="SELECT 1, 2, 3")
     query_id = data["handle"].get("guid")
