611d2668e47cf8ac177591266fb4c0a0c4ff1984
Romain Rigaux <romain.rigaux@gmail.com>
2021-06-21 08:50:04 -0700

    [cli] query API

--- cli/cli/main.py
+++ cli/cli/main.py
@@ -28,6 +28,40 @@ def callback():
 
 
 @app.command()
+def login():
+    import json
+
+    import requests
+
+    session = requests.Session()
+
+    data = {
+        "username": "demo",
+        "password": "demo",
+    }
+
+    response = session.post("https://demo.gethue.com/api/token/auth", data=data)
+    print(
+        "Auth: %s %s"
+        % ("success" if response.status_code == 200 else "error", response.status_code)
+    )
+
+    token = json.loads(response.content)["access"]
+    print("Token: %s" % token)
+
+    response = requests.post(
+        "https://demo.gethue.com/api/query/autocomplete",
+        headers={
+            "Authorization": "Bearer %s" % token,
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data={"snippet": json.dumps({"type": "mysql"})},
+    )
+    print(response.status_code)
+    print(response.text)
+
+
+@app.command()
 def query():
     """
     Execute some SQL statements
